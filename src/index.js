46b035872f2dfab75cb4d2acc2dff898dbf5ade7
Asmodeus <kirill6satur@gmail.com>
2026-07-13 21:00:37 +0300

    JS begin

---
+++ src/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const cityName = document.querySelector('#cityName');
+const cityId = document.querySelector('#cityId');
+const selectedRadioName = document.querySelector('input[value="name"]');
+const selectedRadioId = document.querySelector('input[value="id"]');
+const tempr = document.querySelector('#temperature');
+const windS = document.querySelector('#windSpeed');
+const humid = document.querySelector('#humidity');
+const wBtn = document.querySelector('#weatherBtn');
+const canBtn = document.querySelector('#cancelBtn');
+
